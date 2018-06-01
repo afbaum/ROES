94f44e86727414f80f20a695f7b08bef61723723
Amanda Baum <amanda.baum01@gmail.com>
2018-06-01 14:10:29 -0400

    user authentication

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 //mongodb connection
 mongoose.connect('mongodb://admin:mic530gra@ds259085.mlab.com:59085/hearingaids');
@@ -10,6 +12,21 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 const app = express();
+
+// use sessions for tracking logins
+app.use(session({
+  secret: 'treehouse loves you',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+app.use (function(req, res, next) {
+  res.locals.currentUser = req.session.userId;
+  next();
+});
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,67 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const mongoose = require('mongoose');
+const mid = require('../middleware');
 
 router.get('/', (req, res) => {
   res.render('index');
 });
 
-router.get('/register', (req, res, next) => {
+//GET .profile
+router.get('/profile', mid.requiresLogin, (req, res, next) => {
+  User.findById(req.session.userId)
+  .exec(function (error, user) {
+    if (error) {
+      return next(error);
+    } else {
+      return res.render('profile', {title: 'Profile', name: user.name, email: user.email});
+    }
+  });
+});
+
+//GET /logout
+router.get('/logout', (req, res, next) => {
+  if (req.session) {
+    //delete session object
+    req.session.destroy(function(err) {
+      if(err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
+});
+// GET /login
+router.get('/login', mid.loggedOut, (req, res, next) => {
+    return res.render('login', {title: 'Log In'});
+});
+
+router.post('/login', (req, res, next) => {
+  if (req.body.email && req.body.password){
+    User.authenticate(req.body.email, req.body.password, function(error, user){
+      if (error || !user) {
+        const err = new Error('Wrong email or password.');
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/');
+      }
+    });
+  } else {
+    var err = new Error('Email and password are required.');
+    err.status = 401;
+    return next(err);
+  }
+});
+
+// GET /register
+router.get('/register', mid.loggedOut, (req, res, next) => {
     return res.render('register')
 });
 
+// POST /register
 router.post('/register', (req, res, next) => {
   if (req.body.email &&
     req.body.name &&
@@ -35,10 +87,11 @@ router.post('/register', (req, res, next) => {
         if (error) {
           return next(error);
         } else {
+          req.session.userId = user._id;
           return res.redirect('/');
         }
       });
-      
+
     } else {
       const err = new Error('All fields required');
       err.status = 400;
